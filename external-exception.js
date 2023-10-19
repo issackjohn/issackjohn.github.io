53d9951091202e3d8d1c37e203244dac79670d87
issack john <issackjohn@microsoft.com>
2023-10-19 11:54:04 -0700

    new exception script

---
+++ external-exception.js
@@ -0,0 +1,18 @@
+function externalFoo() {
+  externalBar();
+}
+
+function externalBar() {
+  externalBaz();
+}
+
+function externalBaz() {
+  externalException();
+}
+
+function externalException() {
+  // throw an exception
+  throw new Error("This is an exception");
+}
+
+externalFoo();
